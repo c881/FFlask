15b58c9242996287818b01307afa675148575bf7
c881 <jacov.g@gmail.com>
2021-03-13 05:56:55 +0200

    1st step

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    # return "<h1>Welcome to our server !!</h1>" 1st step
+    # return render_template("index.html") 2nd step
+    # return render_template("index.html", name=request.args.get("name"))
+    return render_template("index.html")
+
+
+@app.route('/greet')
+def greet():
+    return render_template("greet.html", name=request.args.get("name"))
+
+
+if __name__ == '__main__':
+    app.run(threaded=True, port=5000)
