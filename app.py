0223554b0676db3992bd9f46e9f3021d6b9b660c
c881 <jacov.g@gmail.com>
2021-03-14 20:51:38 +0200

    Trying to make expance tracker

--- app.py
+++ app.py
@@ -4,17 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # return "<h1>Welcome to our server !!</h1>" 1st step
-    # return render_template("index.html") 2nd step
-    # return render_template("index.html", name=request.args.get("name"))
+
     return render_template("index.html")
 
-
-@app.route('/greet', methods=["POST"])
-def greet():
-    # return render_template("greet.html", first_name=request.args.get("first_name","world"))
-    # if you use "GET"
-    return render_template("greet.html", first_name=request.form.get("first_name","world"))
 
 
 if __name__ == '__main__':
