b3a55e881fa759d8d586b53f0108f6f2641a67e4
c881 <jacov.g@gmail.com>
2021-03-16 05:24:24 +0200

    Trying to make expance tracker.20210315

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ def index():
 
     return render_template("index.html")
 
+@app.route('/added', methods=["POST"])
+def added():
+    if not request.form.get("sum") or not request.form.get("categories"):
+        return render_template("failure.html")
+    return render_template("success.html")
 
 
 if __name__ == '__main__':
