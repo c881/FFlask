a52fcfe5d8c7842f0fe0b12ae5f4c4c811ed9605
koby <jacov.g@gmail.com>
2021-03-19 18:28:40 +0200

    Index to pep8

--- app.py
+++ app.py
@@ -1,30 +1,33 @@
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
-ERRORS= {"sum":"חסר סכום", "category":"לא בחרת קטגוריה","categories":"קטגוריה לא קיימת"}
-EXPANCES = {}
-CATEGORIES = {"fuel":"דלק" ,"food":"אוכל" ,"education":"חינוך",
-            "holidays":"חגים" ,"insurance":"ביטוח" ,"mortgage":"משכנתה" ,"taxes":"מיסים"}
+ERRORS = {"sum": "חסר סכום", "category": "לא בחרת קטגוריה", "categories": "קטגוריה לא קיימת"}
+EXPANCESS = {}
+CATEGORIES = {"fuel": " דלק", "food": " אוכל", "education": " חינוך",
+              "holidays": "חגים", "insurance": "ביטוח", "mortgage": "משכנתה", "taxes": "מיסים"}
+
+
 @app.route('/')
 def index():
+    return render_template("index.html", categories=CATEGORIES)
 
-    return render_template("index.html",categories=CATEGORIES)
 
 @app.route('/added', methods=["POST"])
 def added():
     # if not request.form.get("sum") or request.form.get("categories") not in CATEGORIES:
     #    return render_template("failure.html")
     # return render_template("success.html")
-    sum = request.form.get("sum")
-    if not sum:
-        return render_template("error.html",message=ERRORS["sum"])
+    sum_input = request.form.get("sum")
+    if not sum_input:
+        return render_template("error.html", message=ERRORS["sum"])
 
-    category = request.form.get("categories"):
+    category = request.form.get("categories")
     if not category:
-        return render_template("error.html",message=ERRORS["category"])
+        return render_template("error.html", message=ERRORS["category"])
 
     if category not in CATEGORIES:
-        return render_template("error.html",message=ERRORS["categories"])
+        return render_template("error.html", message=ERRORS["categories"])
+
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
