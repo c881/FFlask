a09e4dedb06109a70318f3919f6164e8d6799d97
c881 <jacov.g@gmail.com>
2021-03-19 06:04:53 +0200

    Using List/Dictionary to populate an error message.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
+ERRORS= {"sum":"חסר סכום", "category":"לא בחרת קטגוריה","categories":"קטגוריה לא קיימת"}
+EXPANCES = {}
 CATEGORIES = {"fuel":"דלק" ,"food":"אוכל" ,"education":"חינוך",
             "holidays":"חגים" ,"insurance":"ביטוח" ,"mortgage":"משכנתה" ,"taxes":"מיסים"}
 @app.route('/')
@@ -10,10 +12,19 @@ def index():
 
 @app.route('/added', methods=["POST"])
 def added():
-    if not request.form.get("sum") or request.form.get("categories") not in CATEGORIES:
-        return render_template("failure.html")
-    return render_template("success.html")
+    # if not request.form.get("sum") or request.form.get("categories") not in CATEGORIES:
+    #    return render_template("failure.html")
+    # return render_template("success.html")
+    sum = request.form.get("sum")
+    if not sum:
+        return render_template("error.html",message=ERRORS["sum"])
 
+    category = request.form.get("categories"):
+    if not category:
+        return render_template("error.html",message=ERRORS["category"])
+
+    if category not in CATEGORIES:
+        return render_template("error.html",message=ERRORS["categories"])
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
