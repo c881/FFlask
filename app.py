aad5de277e00ce34931ce695bf39329e260672ce
koby <jacov.g@gmail.com>
2021-03-28 22:18:00 +0300

    New columns in DB\index.html

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ db = SQL("sqlite:///sample.db")
 
 valid_chars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.']
 ERRORS = {"sum": "חסר סכום", "category": "לא בחרת קטגוריה", "categories": "קטגוריה לא קיימת",
-          "values": "שדה סכום מכיל תוים מעבר לספרות ו/או/ נקודה"}
+          "values": "שדה סכום מכיל תוים מעבר לספרות ו/או/ נקודה", "date": "תאריך חסר/ לא תקין",
+          "pay": "סחר סוג תשלום/ תשלום שגוי", "num_of_pays": "חסר מספר תשלומים/ תשלומים = 0"}
 
 
 @app.route('/')
@@ -23,7 +24,7 @@ def index():
         return redirect("/login",)
     return render_template("index.html", user_name=session.get("name"),
                            categories=db.execute('SELECT * FROM categories ORDER BY category_id'),
-                           pays=db.execute('SELECT * FROM payTypes ORDER BY TypeID'))
+                           pays=db.execute('SELECT * FROM payTypes ORDER BY PayID'))
 
 
 @app.route('/login', methods=["GET", "POST"])
@@ -52,15 +53,35 @@ def added():
         if char not in valid_chars:
             return render_template("error.html", message=ERRORS["values"])
 
-    category = int(request.form.get("categories"))
-    if not category:
+    category_id = int(request.form.get("categories"))
+    if not category_id:
         return render_template("error.html", message=ERRORS["category"])
 
-    rows = db.execute('SELECT category_id FROM categories where category_id  = (?)', int(category))
+    rows = db.execute('SELECT category_id FROM categories where category_id  = (?)', category_id)
     if len(rows) == 0:
         return render_template("error.html", message=ERRORS["categories"])
 
-    db.execute('INSERT INTO expenses VALUES (?, ?)', int(category), float(sum_input))
+    pay_date = request.form.get("paydate")
+    if not pay_date:
+        return render_template("error.html", message=ERRORS["date"])
+
+    pay_type_id = int(request.form.get("pays"))
+    if not pay_type_id:
+        return render_template("error.html", message=ERRORS["pay"])
+
+    rows = db.execute('SELECT PayId FROM payTypes where PayId  = (?)', pay_type_id)
+    if len(rows) == 0:
+        return render_template("error.html", message=ERRORS["pay"])
+
+    num_of_pays = request.form.get("settlements")
+    if not num_of_pays:
+        return render_template("error.html", message=ERRORS["num_of_pays"])
+    num_of_pays = int(num_of_pays)
+    if num_of_pays == 0:
+        return render_template("error.html", message=ERRORS["num_of_pays"])
+
+    db.execute('INSERT INTO expenses VALUES (?, ?, ?, ?, ?)', category_id, float(sum_input), pay_type_id, num_of_pays,
+               pay_date)
     return redirect("/")
 
 
