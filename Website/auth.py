95d8beac719db8fb8e000dcce21e8659d6a8db41
c881 <jacov.g@gmail.com>
2023-07-14 22:02:18 +0300

    2023.07.14 - SignUp/Login inputs

--- Website/auth.py
+++ Website/auth.py
@@ -1,10 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth',__name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    data = request.form
+    print(data)
     return render_template("login.html")
 
 
@@ -13,6 +15,6 @@ def logout():
     return render_template("logout.html")
 
 
-@auth.route('/sign-up')
+@auth.route('/sign_up', methods=['GET', 'POST'])
 def signup():
-    return render_template("signup.html")
+    return render_template("sign_up.html")
